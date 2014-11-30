12c0054c2f5b732edabce2edd03f9098b37126c0
lhohan <github@lhoest.eu>
2014-11-30 21:02:27 +0100

    wk8 lab - foldLeftM

--- w8.hs
+++ w8.hs
@@ -166,3 +166,12 @@ filterM' p (x:xs)
          ys <- filterM' p xs
          if flag then return (x:ys) else return ys
 
+foldLeftM :: Monad m => (a -> b -> m a) -> a -> [b] -> m a
+
+foldLeftM f a [] = return a
+--foldLeftM f a (x:xs) = f a x >>= \a_ -> foldLeftM f a_ xs
+foldLeftM f a (x:xs) =
+    do a_ <- f a x
+       foldLeftM f a_ xs
+
+--foldLeftM (\a b -> putChar b >> return (b : a ++ [b])) [] "haskell" >>= \r -> putStrLn r
