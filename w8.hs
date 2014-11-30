dfe8a683c991d65f1e5bc77cf34cafd650dc4e9a
lhohan <github@lhoest.eu>
2014-11-30 10:53:04 +0100

    wk8 lab - continued

--- w8.hs
+++ w8.hs
@@ -1,3 +1,27 @@
 putStr' [] = return ()
-putStr' (x : xs) = putChar x >>= putStr' xs
+putStr' (x : xs) = putChar x >> putStr' xs
+
+--putStrLn' [] = putChar '\n'
+--putStrLn' xs = putStr' xs >> putStrLn' ""
+
+--putStrLn' [] = putChar '\n'
+--putStrLn' xs = putStr' xs >> putChar '\n'
+
+--putStrLn' [] = putChar '\n'
+--putStrLn' xs = putStr' xs >>= \x -> putChar '\n'
+
+--putStrLn' [] = putChar '\n'
+--putStrLn' xs = putStr' xs >> \x -> putChar '\n'
+
+--putStrLn' [] = putChar '\n'
+--putStrLn' xs = putStr' xs >> putStr' "\n"
+
+--putStrLn' [] = putChar '\n'
+--putStrLn' xs = putStr' xs >> putStrLn' "\n"
+
+--putStrLn' [] = return ""
+--putStrLn' xs = putStrLn' xs >> putStr' "\n"
+
+putStrLn' [] = putChar "\n"
+putStrLn' xs = putStr' xs >> putChar '\n'
 
