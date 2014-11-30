85fe2e14a531b0c007ee78f2b782d3a88f7c3d28
lhohan <github@lhoest.eu>
2014-11-30 10:33:15 +0100

    savediv in term of Maybe ... messy in ghci , needs overrides and stuff :/

--- monad_hutton.hs
+++ monad_hutton.hs
@@ -13,13 +13,17 @@ sqr_ = map_ (^2)
 data Expr = Val Int | Div Expr Expr
 data Maybe_ a = Nothing_ | Just_ a deriving Show
 
-savediv n m = if m==0 then Nothing_ else Just_(n `div` m)
+safediv n m = if m==0 then Nothing_ else Just_(n `div` m)
+
+apply f Nothing_ = Nothing_
+apply f (Just_ x) = f x
+
+seqn Nothing_    _       = Nothing_
+seqn _          Nothing_ = Nothing_
+seqn (Just_ x)    (Just_ y) = Just_(x,y)
 
 eval            :: Expr -> Maybe_ Int
 eval(Val n)     = Just_ n
-eval(Div x y)   = case eval x of
-                    Nothing_ -> Nothing_
-                    Just_ n -> case eval y of
-                                    Nothing_ -> Nothing_
-                                    Just_ m -> savediv n m
+eval(Div x y)   = apply f (eval x `seqn` eval y)
+                    where f (n,m)= safediv n m
 
