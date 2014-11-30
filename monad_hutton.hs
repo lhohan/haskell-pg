005c4fc1698f6ae0f89029df520ae3c13c94e996
lhohan <github@lhoest.eu>
2014-11-30 09:21:33 +0100

    savediv

--- monad_hutton.hs
+++ monad_hutton.hs
@@ -10,7 +10,16 @@ map_ f (x:xs)    = f(x) : map_ f xs
 inc_ = map_ (+1)
 sqr_ = map_ (^2)
 
-data Expr       = Val Int | Div Expr Expr
-eval(Val n)     = n
-eval(Div x y)   = eval x `div` eval y
+data Expr = Val Int | Div Expr Expr
+data Maybe_ a = Nothing_ | Just_ a deriving Show
+
+savediv n m = if m==0 then Nothing_ else Just_(n `div` m)
+
+eval            :: Expr -> Maybe_ Int
+eval(Val n)     = Just_ n
+eval(Div x y)   = case eval x of
+                    Nothing_ -> Nothing_
+                    Just_ n -> case eval y of
+                                    Nothing_ -> Nothing_
+                                    Just_ m -> savediv n m
 
